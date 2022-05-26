eb1891e5dd66b5898fcb5cbf246fa491109e628d
LMD-02 <mduc040802@gmail.com>
2022-05-26 15:18:29 +0700

    Add project files.

---
+++ TraCuuDieuLuat/Views/Nhom10.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TraCuuDieuLuat.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Nhom10 : ContentPage
+    {
+        public Nhom10()
+        {
+            InitializeComponent();
+        }
+    }
+}
